c8e36445611c97f138f426c817e8c5e18fe9258f
Paul Ibeabuchi C <ibeabuchipaul@gmail.com>
2022-04-26 23:31:42 +0100

    typescript not working yet!

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import logo from './wt-logo.jpg';
 import './App.css';
+import Tile from './components/Tile.tsx'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <Tile />
     </div>
   );
 }
