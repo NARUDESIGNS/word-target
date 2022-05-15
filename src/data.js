64891e07842b538ea920f4d603e6adec8e03a47f
Paul Ibeabuchi C <ibeabuchipaul@gmail.com>
2022-05-15 12:18:37 +0100

    added data source

---
+++ src/data.js
@@ -0,0 +1,15 @@
+const words = [
+    'HAZE', 'GRILL', 'DAMN', 'PILL', 'DEALS', 'OVER', 'EXIT', 'AXIS', 'DRUNK', 'KNEE',
+    'PONDER', 'PRIME', 'ELUDE', 'GORY', 'SKYE', 'ZIP', 'ZOOM', 'DRILLS', 'GEEK', 'FEED',
+    'NOBLE', 'SOUR', 'BIRD', 'ACHE', 'NOTE', 'ESCAPE', 'EXCUSE', 'FICKLE', 'DREAM', 'BLEED',
+    'OZONE', 'BARTER', 'COUCH', 'CRINGE', 'JOT', 'LOVE', 'CLOCK', 'KNOCK', 'KING', 'EVADE',
+    'BREATH', 'VAGUE', 'VOUCH', 'OPINE', 'FLEET', 'WOKE', 'WORRY', 'VANISH', 'VOTE', 'VIM'
+];
+
+const letters = {
+    "Q": 10, "W": 4, "E": 1, "R": 1, "T": 1, "Y": 4, "U": 1, "I": 1, "O":1, "P": 3,
+    "A": 1, "S": 1, "D": 2, "F": 4, "G": 2, "H": 4, "J": 8, "K": 5, "L": 1,
+    "Enter": null, "Z": 10, "X": 8, "C": 3, "V": 4, "B": 3, "N": 1, "M": 3, "Delete": null  
+};
+
+module.exports = { words, letters};
